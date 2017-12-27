1e2e7371012d8ffcddaeed1c9e330e8d20630eb7
I336253 <david.du@sap.com>
2017-12-27 17:20:41 +0800

    add interface

--- src/main/groovy/com/shanshui/smartcommunity/clients/feign/client/api/UserClient.groovy
+++ src/main/groovy/com/shanshui/smartcommunity/clients/feign/client/api/UserClient.groovy
@@ -1,11 +1,42 @@
 package com.shanshui.smartcommunity.clients.feign.client.api
 
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.cloud.netflix.feign.FeignClient
+import org.springframework.web.bind.annotation.*
 
 /**
  * Created by I336253 on 12/27/2017.
  */
-@FeignClient("stores")
+@FeignClient("user")
 public interface UserClient {
+    /**
+     * internal used for services
+     * @param id
+     * @param token
+     * @return
+     */
+    @RequestMapping(value = '/{id}', method = [RequestMethod.GET])
+    //@Cacheable(value = 'user', key = "'#id'")
+    @ResponseBody
+    def get(@PathVariable('id') long id)
 
+    /**
+     * internal used for services
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = '/{id}/token', method = RequestMethod.GET)
+    @Cacheable(value = 'token', key = "'#id'")
+    // cache
+    @ResponseBody
+    def getToken(@PathVariable('id') long id)
+
+    @RequestMapping(value = '/{id}/payment/account', method = RequestMethod.GET)
+    @ResponseBody
+    def getPaymentAccounts(@PathVariable('id') Long id, @RequestHeader(value = "token") String token)
+
+    @RequestMapping(value = '/{id}/payment/account/{pid}', method = RequestMethod.GET)
+    @ResponseBody
+    def getPaymentAccount(
+            @PathVariable('id') long id, @PathVariable('pid') Long pid, @RequestHeader(value = "token") String token)
 }
