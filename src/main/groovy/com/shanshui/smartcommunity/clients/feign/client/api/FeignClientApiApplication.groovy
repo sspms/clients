84e046d7ca65c23b9d7b894ea84dfa648d9ef570
I336253 <david.du@sap.com>
2017-12-27 14:54:21 +0800

    init upload

---
+++ src/main/groovy/com/shanshui/smartcommunity/clients/feign/client/api/FeignClientApiApplication.groovy
@@ -0,0 +1,12 @@
+package com.shanshui.smartcommunity.clients.feign.client.api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FeignClientApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run FeignClientApiApplication, args
+	}
+}
